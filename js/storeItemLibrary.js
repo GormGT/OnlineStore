404f96bb06575c79eb8c45492dcbe8dfebd9edc8
Tayro <adventuregamr@gmail.com>
2022-10-14 14:58:12 +0200

    Added some more items to the store

--- js/storeItemLibrary.js
+++ js/storeItemLibrary.js
@@ -5,7 +5,34 @@ const allItems = [
     {id: 1, name:"Shotgun", image:"../img/StoreShotgun.png", price:"$ 499", quality:"stock", alt:"Bilde av shotgun", },
     {id: 2, name:"Scattergun", image:"../img/StoreScattergun.png", price:"$ 899", quality:"stock", alt:"Bilde av scattergun"},
     {id: 3, name:"Minigun", image:"../img/StoreMinigun.png", price:"$ 250 599", quality:"stock", alt:"Bilde av minigun"},
-    {id: 4, name:"Rainblower", image:"../img/StoreRainblower.png", price:"$ 6 999", quality:"common", alt:"Bilde av rainblower"},
-    {id: 5, name:"Ullapool Caber", image:"../img/StoreUllapoolCaber.png", price:"$ 1 599", quality:"common", alt:"Bilde av ullapool caber"}
+    {id: 4, name:"Flamethrower", image:"../img/StoreFlamethrower.png", price:"$ 6 999", quality:"stock", alt:"Bilde av flamethrower"},
+    {id: 5, name:"Grenade Launcher", image:"../img/StoreGrenadeLauncher.png", price:"$ 4 999", quality:"stock", alt:"Bilde av grenade launcher"},
+    {id: 6, name:"Rocket Launcher", image:"../img/StoreRocketLauncher.png", price:"$ 9 399", quality:"stock", alt:"Bilde av rocket launcher"},
+    {id: 7, name:"Pistol", image:"../img/StorePistol.png", price:"$ 199", quality:"stock", alt:"Bilde av pistol"},
+    {id: 8, name:"Revolver", image:"../img/StoreRevolver.png", price:"$ 199", quality:"stock", alt:"Bilde av revolver"},
+    {id: 9, name:"Medigun", image:"../img/StoreMedigun.png", price:"$ 11 899", quality:"stock", alt:"Bilde av medigun"},
+    {id: 10, name:"Wrench", image:"../img/StoreWrench.png", price:"$ 19", quality:"stock", alt:"Bilde av wrench"},
+    {id: 11, name:"Sniper Rifle", image:"../img/StoreSniperRifle.png", price:"$ 899", quality:"stock", alt:"Bilde av sniper rifle"},
+    {id: 12, name:"Shovel", image:"../img/StoreShovel.png", price:"$ 59", quality:"stock", alt:"Bilde av shovel"},
+    {id: 13, name:"Stickybomb Launcher", image:"../img/StoreStickybombLauncher.png", price:"$ 8 999", quality:"stock", alt:"Bilde av stickybomb launcher"},
+    {id: 14, name:"Frying pan", image:"../img/StoreFryingPan.png", price:"$ 29", quality:"stock", alt:"Bilde av stekepanne"},
+    {id: 40, name:"Rainblower", image:"../img/StoreRainblower.png", price:"$ 6 999", quality:"common", alt:"Bilde av rainblower"},
+    {id: 57, name:"Ullapool Caber", image:"../img/StoreUllapoolCaber.png", price:"$ 1 599", quality:"common", alt:"Bilde av ullapool caber"}
     //ToDo: Add further stock items
 ];
+
+/*
+
+To add (from image folder):
+-Sapper
+-Syringe Gun
+-Bottle
+-Fire axe
+-Bat
+-Holy mackerel
+-Knife
+-SMG
+-Kukri
+-Bonesaw
+
+*/
