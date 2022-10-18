9636e477d763409d3f4410944ede1a2d0a223572
Tayro <adventuregamr@gmail.com>
2022-10-18 10:35:07 +0200

    Improved index styles

--- js/storeItemLibrary.js
+++ js/storeItemLibrary.js
@@ -15,7 +15,7 @@ const allItems = [
     {id: 11, name:"Sniper Rifle", image:"../img/StoreSniperRifle.png", price:"$ 899", quality:"stock", alt:"Bilde av sniper rifle"},
     {id: 12, name:"Shovel", image:"../img/StoreShovel.png", price:"$ 59", quality:"stock", alt:"Bilde av shovel"},
     {id: 13, name:"Stickybomb Launcher", image:"../img/StoreStickybombLauncher.png", price:"$ 8 999", quality:"stock", alt:"Bilde av stickybomb launcher"},
-    {id: 14, name:"Frying pan", image:"../img/StoreFryingPan.png", price:"$ 29", quality:"stock", alt:"Bilde av stekepanne"},
+    {id: 14, name:"Frying pan", image:"../img/StoreFryingPan.png", price:"$ 29", quality:"common", alt:"Bilde av stekepanne"},
     {id: 15, name:"Kukri", image:"../img/StoreKukri.png", price:"$ 79", quality:"stock", alt:"Bilde av kukri"},
     {id: 16, name:"Syringe Gun", image:"../img/StoreSyringeGun.png", price:"$ 250", quality:"stock", alt:"Bilde av Syringe gun"},
     {id: 17, name:"Bottle", image:"../img/StoreBottle.png", price:"$ 286", quality:"stock", alt:"Bilde av bottle"},
@@ -25,7 +25,7 @@ const allItems = [
     {id: 21, name:"Knife", image:"../img/StoreKnife.png", price:"$ 500", quality:"stock", alt:"Bilde av kniv"},
     {id: 22, name:"SMG", image:"../img/StoreSMG.png", price:"$ 189", quality:"stock", alt:"Bilde av SMG"},
     {id: 23, name:"Bonesaw", image:"../img/StoreBonesaw.png", price:"$ 149", quality:"stock", alt:"Bilde av beinsag"},
-    {id: 24, name:"Holy Mackerel", image:"../img/StoreHolyMackerel.png", price:"$ 100", quality:"stock", alt:"Bilde av holy mackerel"},
+    {id: 24, name:"Holy Mackerel", image:"../img/StoreHolyMackerel.png", price:"$ 100", quality:"common", alt:"Bilde av holy mackerel"},
     {id: 40, name:"Rainblower", image:"../img/StoreRainblower.png", price:"$ 6 999", quality:"common", alt:"Bilde av rainblower"},
     {id: 57, name:"Ullapool Caber", image:"../img/StoreUllapoolCaber.png", price:"$ 1 599", quality:"common", alt:"Bilde av ullapool caber"}
     //ToDo: Add further stock items
