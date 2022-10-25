5a50e3b92c8d832329791250febd73717bfc7499
Tayro <adventuregamr@gmail.com>
2022-10-25 15:17:22 +0200

    Categories on the products page

--- js/index.js
+++ js/index.js
@@ -1,34 +1,33 @@
-/* ---------------Functionality for the index image carousel---------- */
+const sortDropdown = document.getElementById("sortDropdown");
+const typeDropdown = document.getElementById("typeDropdown");
+const qualityDropdown = document.getElementById("qualityDropdown");
 
-let slideIndex = 1;
-showSlides(slideIndex);
+function showDropdown(dropdown){
+    if (dropdown == 1){
+        sortDropdown.classList.toggle("show");
+        typeDropdown.classList.remove("show");
+        qualityDropdown.classList.remove("show");
+    } else  if (dropdown == 2){
+        sortDropdown.classList.remove("show");
+        typeDropdown.classList.toggle("show");
+        qualityDropdown.classList.remove("show");
+    } else  if (dropdown == 3){
+        sortDropdown.classList.remove("show");
+        typeDropdown.classList.remove("show");
+        qualityDropdown.classList.toggle("show");
+    }
+}//This is a serious ducttape solution, however it works for now while I work on finishing everything else
 
-// Switches between images through the arrows
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-  //console.log(slideIndex);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-//The main function which changes between images
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("carouselElement");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropdownToggle') && !event.target.matches('.dropdownContent') && !event.target.matches('.dropdownContent input') && !event.target.matches('.dropdownContent label') && !event.target.matches('.dropdownContent p')) { //This is giving me a love hate relationship with W3 schools. Their code works really well for what it's designed for, but if you try to modify it slightly, you have to start adding ducttape solutions
+      var dropdowns = document.getElementsByClassName("dropdownContent");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
   }
-  slides[slideIndex-1].style.display = "block";
-}
-
-
-//Automatically runs the plusSlides function to automate the slides
-setInterval( () => {
-    plusSlides(1);
-}, 7000);
-
