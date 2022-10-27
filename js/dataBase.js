91b5cc88824ab8784195880f5bfad1f0e9952b2b
Tayro <adventuregamr@gmail.com>
2022-10-27 10:22:16 +0200

    Integration of Firebase to main index page

---
+++ js/dataBase.js
@@ -0,0 +1,7 @@
+/*db.collection('storeItems').get().then((snapshot) => {
+    snapshot.docs.forEach(doc => {
+        console.log(doc.data())
+    })
+}).catch((err) => {
+    console.log(err)
+});*/
--- js/storeSort.js
+++ js/storeSort.js
@@ -1,14 +1,39 @@
-let storeSlots = Array.from(document.querySelector(".indexStoreArea").children);
+//-------------------------------------------------------------Main database storesort--------------------------------------------------------
+let storeSlots = document.querySelector(".storeArea");
 
+const addStoreItem = (storeItem) => {
+    let itemHtml =`
+    <div class="storeItem">
+        <p class="itemName">${storeItem.itemName}</p>
+        <img src="" height="120px" alt="">
+        <p class="price">${storeItem.itemPrice}</p>
+    </div>
+    `;
+    storeSlots.innerHTML += itemHtml;
+}
+
+
+db.collection('storeItems').get().then((snapshot) => {
+    snapshot.docs.forEach(doc => {
+        addStoreItem(doc.data());
+        //i++;
+    })
+}).catch((err) => {
+    console.log(err)
+});
+
+//-------------------------------------------------------------Fallback storesort--------------------------------------------------------
+
+//let storeSlots = Array.from(document.querySelector(".indexStoreArea").children);
 
 //let storeSlotText = document.querySelectorAll(".storeItem p");
 //let storeSlotPrice = document.querySelectorAll(".storeItem p.price");
 //let storeSlotImage = document.querySelectorAll(".storeItem img");
 
-function findStoreItems(){
+function findFallbackStoreItems(){
 
 
-    function fillArray(){
+    function fillFallbackArray(){
         let itemArray = [];
         for(i = 1; i < allItems.length + 1; i++){
             itemArray.push(i);
@@ -16,29 +41,29 @@ function findStoreItems(){
         return itemArray;
     }
 
-    function assignID(objs){
-        let ids = fillArray();
+    function assignFallbackID(objs){
+        let ids = fillFallbackArray();
         objs.forEach(obj => {
             let randomID = Math.floor(Math.random() * ids.length);
             obj.id = ids[randomID]
             ids.splice(randomID, 1)
         })
     }
-    assignID(allItems);
+    assignFallbackID(allItems);
     allItems.sort((a, b) => a.id - b.id);
-    console.log(allItems);
+    //console.log(allItems);
 }
 
-findStoreItems();
+//findStoreItems();
 
-function addStoreItems(allStoreSlots, objects){
+function addFallbackStoreItems(allStoreSlots, objects){
     for(i = 0; i < allStoreSlots.length; i++){
         const slot = allStoreSlots[i];
-        console.log(slot);
+        //console.log(slot);
         const object = objects[i];
-        console.log(object);
+        //console.log(object);
         const itemName = slot.querySelector(".itemName");
-        console.log(itemName);
+        //console.log(itemName);
         const image = slot.querySelector("img");
         const price = slot.querySelector("p.price");
         itemName.innerText = object.name;
@@ -47,4 +72,4 @@ function addStoreItems(allStoreSlots, objects){
     };
 }
 
-addStoreItems(storeSlots, allItems);
+//addStoreItems(storeSlots, allItems);
